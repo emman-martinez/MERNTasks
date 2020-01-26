6bbb8176d6db3b237a89669216943fbd678d599a
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-01-26 17:22:59 -0600

    Componentes, state y funciones: Login, Crear Cuenta y Proyectos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
+        <Route exact path="/proyectos" component={Proyectos}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+
+/*
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,3 +47,5 @@ function App() {
 }
 
 export default App;
+
+*/
