392bf04119fa1a1089660102948f3ad9683ad451
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-01-29 22:30:31 -0600

    Mostrando el Formulario de NuevosProyectos de forma condicional

--- frontend/src/components/proyectos/NuevoProyecto.js
+++ frontend/src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import proyectoContext from './../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { formulario } = proyectosContext;
 
     // State para proyecto
     const[proyecto, guardarProyecto] = useState({
@@ -35,24 +40,31 @@ const NuevoProyecto = () => {
             >
                 Nuevo Proyecto
             </button>
-            <form
-                className="formulario-nuevo-proyecto"
-                onSubmit={onSubmitProyecto}
-            >
-                <input 
-                    type="text"
-                    className="input-text"
-                    placeholder="Nombre Proyecto"
-                    name="nombre"
-                    value={nombre}
-                    onChange={onChangeProyecto}
-                />
-                <input 
-                    type="submit"
-                    className="btn btn-primario btn-block"
-                    value="Agregar Proyecto"
-                />
-            </form>
+            {
+                formulario
+                ?
+                    (
+                        <form
+                            className="formulario-nuevo-proyecto"
+                            onSubmit={onSubmitProyecto}
+                        >
+                            <input 
+                                type="text"
+                                className="input-text"
+                                placeholder="Nombre Proyecto"
+                                name="nombre"
+                                value={nombre}
+                                onChange={onChangeProyecto}
+                            />
+                            <input 
+                                type="submit"
+                                className="btn btn-primario btn-block"
+                                value="Agregar Proyecto"
+                            />
+                        </form>
+                    )
+                : null
+            }
         </Fragment> 
     );
 }
