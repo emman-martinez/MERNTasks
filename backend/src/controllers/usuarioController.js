6e24124b64b4afea2565bc168fcbfdad2d47ae8b
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-02-06 22:07:47 -0600

    Insertando Usuarios en BD, Validación de Usuario único, Hashear password en la BD

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -2,11 +2,40 @@
 const usuariosCtrl = {};
 
 const Usuario = require('./../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
-// Función: POST - CREATE
+// ***** POST - CREATE => crearUsuario ***** //
 usuariosCtrl.crearUsuario = async(req, res) => {
+
     console.log('Desde usuarioController: crearUsuario');
     console.log(req.body);
+
+    // Extraer Email y Password 
+    const { email, password } = req.body;
+
+    try {
+
+        let usuario = await Usuario.findOne({ email }); // Revisar que el usuario registrado sea único
+
+        if (usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
+
+        usuario = new Usuario(req.body); // Crea el nuevo usuario
+
+        const salt = await bcryptjs.genSalt(10); // Hashear el password
+        usuario.password = await bcryptjs.hash(password, salt);
+
+        await usuario.save(); // Guardar Usuario
+        res.json({ msg: 'Usuario Creado Correctamente' }); // Mensaje de confirmación  
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(400).send('Hubo un Error');
+
+    }
+
 };
 
 module.exports = usuariosCtrl;
